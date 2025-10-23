5844744e533b013a2838c576aed836a5ae1e951e
Anthony Xavier <anthonyltx12@gmail.com>
2025-10-23 15:53:28 -0300

    Criado o user repository

---
+++ auth/auth/src/main/java/com/xavier/auth/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.xavier.auth.repositories;
+
+import com.xavier.auth.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository <User, String> {
+    UserDetails findByLogin(String login);
+}
